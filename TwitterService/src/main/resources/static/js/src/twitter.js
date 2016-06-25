9cf0f0ae2632c19541cc9952557c73ff87554acb
Vamsy <vamsyraju@gmail.com>
2016-06-24 22:29:21 -0500

    Cleaned pom file

--- TwitterService/src/main/resources/static/js/src/twitter.js
+++ TwitterService/src/main/resources/static/js/src/twitter.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	 } )
 	 .DataTable( {
 	 ajax: {
-         url: "http://localhost:8080/getTimeLines?userName=salesforce&sizeLimit=20",
+         url: "http://localhost:8080/getTimeLines?userName=salesforce&sizeLimit=10",
          type: "GET",
      },
      columns: [
